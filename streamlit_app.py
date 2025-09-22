bd7748228ae0ecf6568afbbcb1a224e23548434e
Mephastopholies <n8.wolf82@gmail.com>
2025-09-22 09:12:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,12 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
-
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
     
         st.success('Your Smoothie is ordered!', icon='👍')
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
