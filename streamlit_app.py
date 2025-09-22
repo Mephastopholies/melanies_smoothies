d3b5dafe16aa08bd13d8a1dc8e9a3261aba21757
Mephastopholies <n8.wolf82@gmail.com>
2025-09-22 09:51:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,23 +26,23 @@ ingredients_list = st.multiselect(
     )
 
 if ingredients_list:
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    #st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) 
-        values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
-
+ 
+    ingredients_str = ''
+ 
+    for fruit in ingredients_list:
+        ingredients_str += fruit + ' '
+ 
+    #st.write(ingredients_str)
+ 
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_str + """','"""+name_on_order+"""')"""
+ 
     #st.write(my_insert_stmt)
+ 
     time_to_insert = st.button('Submit Order')
-
+ 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-    
         st.success('Your Smoothie is ordered!', icon='👍')
 
 import requests
