ff7e0414a458fe616de62446611508de95673f51
Mephastopholies <n8.wolf82@gmail.com>
2025-09-30 11:21:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(date=fruityvice_response.json(). use_container_width=true)
+        fv_df = st.dataframe(data=fruityvice_response.json(). use_container_width=true)
         
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_str + """','"""+name_on_order+"""')"""
